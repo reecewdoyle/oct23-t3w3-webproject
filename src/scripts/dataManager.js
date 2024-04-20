f8ac6c24812da8e46c992857efae78a51cd18377
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-04-20 10:39:54 +1000

    fix: add startup behaviour for local storage data

--- src/scripts/dataManager.js
+++ src/scripts/dataManager.js
@@ -40,6 +40,30 @@ function setCssThemesToStorage(){
 	localStorage.setItem("cssThemes", dataAsJsonString);
 }
 
+function setPageThemeToStorage(){
+    localStorage.setItem("pageTheme", pageTheme);
+}
+
+
+
+// Check if data exists,
+// if it does, retrieve it,
+// else, set default variables to localstorage
+if (localStorage.getItem("pageTheme") && localStorage.getItem("cssThemes").length > 0){
+    getStoredCssThemes();
+    getStoredPageTheme();
+} else {
+    setCssThemesToStorage();
+    setPageThemeToStorage();
+}
+
+
+
+
+
+
+
+
 
 /*
 
